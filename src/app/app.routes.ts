58acf9257a3d2735da40c654b26730322796b812
isuru <isuruliyanageofficial@gmail.com>
2024-11-25 11:40:30 +0530

    finalized

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {LandingComponent} from './landing/landing.component';
+import {AddItemComponent} from './add-item/add-item.component';
+import {ChangeCredentialsComponent} from './change-credentials/change-credentials.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingComponent },
+  {path:'dashboard',component: DashboardComponent},
+  {path:'add_item',component: AddItemComponent},
+  {path:'change_pw',component:ChangeCredentialsComponent}
+];
--- src/app/modules.ts
+++ src/app/modules.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {RouterOutlet} from '@angular/router';
+import {RouterModule, RouterOutlet} from '@angular/router';
 import {MatCardModule} from '@angular/material/card';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -8,19 +8,49 @@ import {MatInputModule} from '@angular/material/input';
 import {MatDivider} from '@angular/material/divider';
 import {MatButton, MatButtonModule} from '@angular/material/button';
 import {MatDialogModule} from '@angular/material/dialog';
+import {SideBarComponent} from './side-bar/side-bar.component';
+import {MatSelect} from '@angular/material/select';
+import {MatOption} from '@angular/material/core';
+import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/material/datepicker';
+import {MatCheckbox} from '@angular/material/checkbox';
+import {
+  MatCell, MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef,
+  MatRow, MatRowDef,
+  MatTable
+} from '@angular/material/table';
+import {MatIcon} from '@angular/material/icon';
+import {AngularToastifyModule} from 'angular-toastify';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterOutlet,
+    RouterModule,
     MatCardModule,
-    FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatDivider, MatButton,MatDialogModule, MatButtonModule
+    MatSelect,
+    MatOption,
+    MatDatepickerInput,
+    MatDatepickerToggle,
+    MatCheckbox,
+    AngularToastifyModule,
+    FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatDivider, MatButton,MatDialogModule, MatButtonModule,MatTable, MatOption, MatSelect, MatHeaderCell, MatCell, MatIcon, MatHeaderRow, MatRow, MatColumnDef, MatHeaderCellDef, MatCellDef, MatHeaderRowDef, MatRowDef
   ],
   exports: [
     CommonModule,
     RouterOutlet,
     MatCardModule,
-    FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatDivider, MatButton,MatDialogModule, MatButtonModule
+    RouterModule,
+    MatSelect,
+    MatOption,
+    MatDatepickerInput,
+    MatDatepickerToggle,
+    MatCheckbox,
+    AngularToastifyModule,
+    FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatDivider, MatButton,MatDialogModule, MatButtonModule,MatTable, MatOption, MatSelect, MatHeaderCell, MatCell, MatIcon, MatHeaderRow, MatRow, MatColumnDef, MatHeaderCellDef, MatCellDef, MatHeaderRowDef, MatRowDef
   ],
 })
 export class SharedModules {}
